6c3eacdbfe52655d5e34b18a7c43d2056642f073
Asranov <asranov0003@gmail.com>
2023-08-05 11:40:54 +0500

    home added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
-    <div className='home'>
-      <p>home</p>
+    <div className='home' id='home'>
+      <div className="home__container">
+        <h1 className="home__title">
+          <b>Asranov</b>
+          <span style={{ fontWeight: 500 }}> Bakhtnur</span>
+          <br />
+          <span className="home__desc" style={{ fontSize: '1.5rem' }}>
+            Software Engineer
+          </span>
+          <br />
+        </h1>
+      </div>
     </div>
   )
 }
