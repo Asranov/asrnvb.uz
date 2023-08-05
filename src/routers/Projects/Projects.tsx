7671021bf607f3d5cda1ee14002865146f23551d
Asranov <asranov0003@gmail.com>
2023-08-05 18:49:44 +0500

    changed favicon and title

--- src/routers/Projects/Projects.tsx
+++ src/routers/Projects/Projects.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import "./Projects.css"
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <div className="projects__container">
+        <div className='projects__page-title'>
+          <p>
+            Welcome to my <span>projects</span> page!
+            While I'm still working on showcasing my <span>projects</span> here,
+            I wanted to give you a glimpse of what's to come.
+            I'm excited to share the details of these <span>projects</span> once they're ready.
+            Stay tuned for updates and new additions.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
